a902e27191e207c2e49a64242ff2c0793eb2ebf2
Nicola Hastings <nicolahastings@gmail.com>
2021-06-22 14:01:53 -0400

    NH | fixes for blog display

--- nsh-blog-viewer.js
+++ nsh-blog-viewer.js
@@ -1,19 +1,67 @@
 $(function () {
-    var blogPost = $('.blogs-block'),
-        tags = $('.hltags');
 
-    $(tags).prependTo(blogPost);
+    var blogPost = $('.blogs-block');
+    // handle page title
+    
+    if (!$('.blogTitle').html()) {
+        var interiorHeadingText = $('h1#PageTitleH1').text();
+    
+        $('h1#PageTitleH1').text('Fixation on Histology Blog');
+    
 
-    $(tags).wrap('<div class="blog-tags" />');
+        $(blogPost).prepend('<h3 class="blogTitle" >' + interiorHeadingText + '</h3>');
+    }
 
-    $(tags).find('li').each(function () {
-        var self = $(this);
+    // $($('div[id*="Permission_ByLinePanel"]')).insertAfter($('.blog-title-text'));
 
-        console.log('>>>>>>>>>>>>>> ', $(self).attr('aria-label'));
-    });
-
+    // handle likes and tags
     var likes = $('.ItemRatingCommentPanel');
 
-    $(likes).appendTo('.blog-tags');
+    $(likes).prependTo(blogPost);
 
+    $(likes).wrap('<div class="blog-tags" />');
+
+    $('.user-content-hashtag').each(function () {
+        var self = $(this),
+            text = $(self).text();
+
+        text = $.trim(text);
+        text = text.substring(1, text.length);
+
+        $(self).text(text);
+    });
+
+    $('.user-content-hashtag').wrapAll('<div class="tags-list" />');
+    $('.tags-list').prependTo('.blog-tags');
+
+    // handle byline
+
+    var byline = $(blogPost).find('div[id*="ByLinePanel"]');
+
+    $(byline).wrapInner('<div class="byline-date" />');
+    $(byline).find('a').prependTo(byline);
+    $(byline).find('.TitleHeading:first-child').prependTo(byline);
+
+    $(byline).find('> .TitleHeading').wrap('<div class="byline-name" />');
+    $(byline).find('a').appendTo($(byline).find('.byline-name'));
+    
+    $('.byline-date').find('.TitleHeading').remove();
+    
+    var bylineDate = $(byline).find('.byline-date').text();
+
+    bylineDate = $.trim(bylineDate);
+    bylineDate = bylineDate.substring(0, (bylineDate.indexOf(':') - 2));
+
+    $(byline).find('.byline-date').text(bylineDate);
+
+    // remove nbsp
+
+    var nbspContainer = $(byline).closest('.form-inline').html(),
+        replacement = new RegExp(String.fromCharCode(160), 'g');
+
+    nbspContainer = $.trim(nbspContainer);
+
+   nbspContainer =  nbspContainer.replace(/&nbsp;/g, '');
+
+   $(byline).closest('.form-inline').html(nbspContainer);
 });
